6331f23b53c23fd68535b35fea2b36ebdee6f810
Zahra Hanifehlou <61181987+zahrahanifehlou@users.noreply.github.com>
2026-05-05 14:42:08 +0200

    add working-directory

--- app/train.py
+++ app/train.py
@@ -2,13 +2,10 @@ import pandas as pd
 from model import train_model, save_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import os
 
 def main():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    data_path = os.path.join(base_dir, "..", "data", "iris.csv")
-
-    data = pd.read_csv(data_path)
+    
+    data = pd.read_csv("data/iris.csv")
 
     X = data.drop("Species", axis=1)
     y = data["Species"]
